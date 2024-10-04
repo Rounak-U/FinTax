e49a794e5714d6799c310f51e9c0658d69cb5dbd
Rounak-U <162772026+Rounak-U@users.noreply.github.com>
2024-10-04 13:58:19 +0530

    Create TaxHome.js

---
+++ TaxHome.js
@@ -0,0 +1,47 @@
+const sectionTitle = document.querySelector('.services-section h2');
+const cards = document.querySelectorAll('.services-section .card');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            if (entry.target === sectionTitle) {
+                // Animate h2
+                entry.target.classList.add('show');
+            } else {
+                // Apply a delay for cards based on their index
+                entry.target.style.setProperty('--delay', `${index * 0.16}s`);
+                entry.target.classList.add('show');
+            }
+        }
+    });
+});
+
+// Attach the observer to the h2 and each card
+observer.observe(sectionTitle);
+cards.forEach(card => {
+    observer.observe(card);
+});
+
+
+const sectionTitle1 = document.querySelector('.why-choose-section h2');
+const cards1 = document.querySelectorAll('.why-choose-section .card');
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            if (entry.target === sectionTitle) {
+                // Animate h2 
+                entry.target.classList.add('show');
+            } else {
+                // Apply a delay for cards based on their index
+                entry.target.style.setProperty('--delay', `${index * 0.16}s`);
+                entry.target.classList.add('show');
+            }
+        }
+    });
+});
+
+// Attach the observer to the h2 and each card
+observer1.observe(sectionTitle1);
+cards1.forEach(card1 => {
+    observer.observe(card1);
+});
