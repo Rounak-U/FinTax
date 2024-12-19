5365633f40cdc75af72add1394df9f212891d5f4
Rounak-U <162772026+Rounak-U@users.noreply.github.com>
2024-12-19 22:55:34 +0530

    Update support.js

--- support.js
+++ support.js
@@ -1,4 +1,3 @@
-// Select the h2 and p elements
 const sectionTitle = document.querySelector('.support h2');
 const sectionParagraph = document.querySelector('.support p');
 
@@ -8,34 +7,32 @@ const observer = new IntersectionObserver(entries => {
             entry.target.classList.add('show');
             if (entry.target === sectionTitle) {
                 setTimeout(() => {
-                    sectionParagraph.classList.add('show'); 
-                }, 10); 
+                    sectionParagraph.classList.add('show');
+                }, 10);
             }
         }
     });
 });
 
 observer.observe(sectionTitle);
-
 const detailsTitle = document.querySelector('.details h5');
-
 const detailsParagraphs = document.querySelectorAll('.details p');
+
 
 const observer1 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             if (entry.target === detailsTitle) {
                 entry.target.classList.add('show');
-
                 detailsParagraphs.forEach((paragraph, index) => {
                     setTimeout(() => {
-                        paragraph.classList.add('show'); 
-                    }, 50 + index * 100); 
+                        paragraph.classList.add('show');
+                    }, 50 + index * 100);
                 });
             }
         }
     });
-}, { threshold: 0.5 }); 
+}, { threshold: 0.5 });
 
 observer1.observe(detailsTitle);
 
